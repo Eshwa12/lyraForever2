7d0a41534469d757347f5cf9609527f790ca01f6
Arki_cydeo <aazar412@gmail.com>
2023-03-08 20:20:14 -0500

    US9 Test

--- src/test/java/com/lyraForever2/runners/CukesRunner.java
+++ src/test/java/com/lyraForever2/runners/CukesRunner.java
@@ -13,8 +13,8 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/lyraForever2/step_definitions",
-        dryRun = false,
-        tags = "",
+        dryRun = true,
+        tags = "@B28G27-110",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
